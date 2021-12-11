6d687f3a137866149464f9d401fc1c7812210436
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-11 15:19:18 +0900

    [Add] First commit

---
+++ MVVM-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM-SwiftUI
+//
+//  Created by Alan Liu on 2021/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM-SwiftUI/MVVM_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUI_App.swift
+//  MVVM-SwiftUI
+//
+//  Created by Alan Liu on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

af6c60e1e8dd0d7259236e9fe1e6221b5da8875c
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-11 15:34:28 +0900

    [Add] Recipe

---
+++ MVVM-SwiftUI/Models/Recipe.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe.swift
+//  MVVM-SwiftUI
+//
+//  Created by Alan Liu on 2021/12/11.
+//
+
+import Foundation
+
+struct Recipe: Identifiable {
+    var id = UUID()
+    var name = ""
+    var cuisine = ""
+}
